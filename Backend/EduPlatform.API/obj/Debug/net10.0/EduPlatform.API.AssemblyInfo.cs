fbc7cc4ae16844a67cc49209e10302fc8715a9f7
Amer Timraz <amertimraz@email.com>
2026-03-11 10:57:58 +0200

    Add gitignore

--- Backend/EduPlatform.API/obj/Debug/net10.0/EduPlatform.API.AssemblyInfo.cs
+++ Backend/EduPlatform.API/obj/Debug/net10.0/EduPlatform.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EduPlatform.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f42003103fd4df0987697a9102257fc7830c0bb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("EduPlatform.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EduPlatform.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
