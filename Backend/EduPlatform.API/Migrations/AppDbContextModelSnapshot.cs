c7d52ee23ef126b9e637a8d266a30c42957a5162
Amer Timraz <amertimraz@email.com>
2026-05-06 11:26:47 +0300

    Downgrade to .NET 8.0 with compatible EF Core packages

--- Backend/EduPlatform.API/Migrations/AppDbContextModelSnapshot.cs
+++ Backend/EduPlatform.API/Migrations/AppDbContextModelSnapshot.cs
@@ -15,7 +15,7 @@ namespace EduPlatform.API.Migrations
         protected override void BuildModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
-            modelBuilder.HasAnnotation("ProductVersion", "10.0.0");
+            modelBuilder.HasAnnotation("ProductVersion", "8.0.10");
 
             modelBuilder.Entity("EduPlatform.API.Models.AppSetting", b =>
                 {
